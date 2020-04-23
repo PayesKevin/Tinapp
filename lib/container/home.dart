52618a8a9b47f3fbddc5334f62cbaec6c0759aec
PayesKevin <38531958+PayesKevin@users.noreply.github.com>
2020-04-22 21:15:21 -0600

    versio1.0
    ahi esta xD :v

---
+++ lib/container/home.dart
@@ -0,0 +1,12 @@
+ 
+import 'package:flutter/material.dart';
+import 'package:tinsearch_projecto/container/tienda.dart';
+
+class HomePage extends StatelessWidget {
+  
+  @override
+    Widget build(BuildContext context) {
+      return EcommerceTwoPage();
+    }
+}
+
